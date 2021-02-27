881b81d2c3a5816199eda4a5d9cac4a9735f818e
Emerson Reis <emersonreisdacosta@gmail.com>
2021-02-27 17:27:29 -0400

    criado o metodo GET com @PathVariable id

--- src/main/java/com/buriti/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/buriti/codeblog/controller/CodeBlogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.buriti.codeblog.model.Post;
@@ -14,11 +15,20 @@ import com.buriti.codeblog.model.service.CodeBlogService;
 public class CodeBlogController {
 	@Autowired
 	CodeBlogService codeBlogService;
+	
 	@GetMapping(value = "/posts")
 	public ModelAndView  getPost() {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = codeBlogService.findAll();
 		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	@GetMapping(value = "/posts/{id}")
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = codeBlogService.findById(id);
+		mv.addObject("post",post);
 		return mv;
 	}
 }
