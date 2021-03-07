41b70abb801c4669187eeff59ce4d39903c2fdb9
Emerson Reis <emersonreisdacosta@gmail.com>
2021-03-07 00:27:35 -0400

    adicionado upload de imagens e o atributo nomeImagem na classe post

--- src/main/java/com/buriti/buritiscript/controller/BuritiScriptController.java
+++ src/main/java/com/buriti/buritiscript/controller/BuritiScriptController.java
@@ -1,5 +1,10 @@
 package com.buriti.buritiscript.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -8,10 +13,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,55 +27,82 @@ import com.buriti.buritiscript.model.service.BuritiScriptService;
 
 @Controller
 public class BuritiScriptController {
+	private static String caminhoImg = "/home/emerson/Imagens/buritiScript/img/";
+
 	@Autowired
 	BuritiScriptService buritiScriptService;
-	
+
 	@GetMapping
 	public String redirectToPosts() {
 		return "redirect:/posts";
+
 	}
-	
+
 	@GetMapping(value = "/posts")
-	public ModelAndView  getPost() {
+	public ModelAndView getPost() {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = buritiScriptService.findAll();
 		mv.addObject("posts", posts);
-		
+
 		return mv;
 	}
-	
+
 	@GetMapping(value = "/posts/{id}")
 	public ModelAndView getPostDetails(@PathVariable("id") long id) {
 		ModelAndView mv = new ModelAndView("postDetails");
 		Post post = buritiScriptService.findById(id);
-		mv.addObject("post",post);
-		
+		mv.addObject("post", post);
+
 		return mv;
 	}
-	
+
 	@GetMapping(value = "/newPost")
-	public ModelAndView getPostForm(){
+	public ModelAndView getPostForm() {
 		ModelAndView mv = new ModelAndView("postForm");
 		mv.addObject(new Post());
 		return mv;
 	}
-	
+
 	@PostMapping(value = "newPost")
-	public ModelAndView savePost(@Valid Post post, BindingResult result,
-			RedirectAttributes attributes,RedirectAttributes redirect,
-			Errors erro) {
+	public ModelAndView savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes,
+			RedirectAttributes redirect, @RequestParam("imagem") MultipartFile imagem) {
+
 		post.setData(LocalDate.now());
 		ModelAndView mv = new ModelAndView("postForm");
-		mv.addObject(post);
-		if(result.hasErrors()) {
+
+		if (result.hasErrors()) {
 			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
 			return mv;
 		}
-		
+		mv.addObject(post);
+
+		buritiScriptService.save(post);
+		try {
+			if (!imagem.isEmpty()) {
+				byte[] bytes = imagem.getBytes();
+				Path caminho = Paths.get(caminhoImg + String.valueOf(post.getId()) + imagem.getOriginalFilename());
+				Files.write(caminho, bytes);
+
+				post.setNomeImagem(String.valueOf(post.getId()) + imagem.getOriginalFilename());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		buritiScriptService.save(post);
 		mv.setViewName("redirect:/posts");
 		return mv;
 	}
-	
-	
+	 
+	@GetMapping("/posts/mostrarImagem/{imagem}")
+	@ResponseBody
+	public byte[] retornarImagem(@PathVariable("imagem") String imagem) throws IOException {
+		File imagemArquivo = new File(caminhoImg +imagem);
+		if(imagem!=null || imagem.trim().length()>0) {
+		
+			return Files.readAllBytes(imagemArquivo.toPath());
+		
+		}
+		return null;
+		
+	}
 }
--- src/main/java/com/buriti/buritiscript/model/Post.java
+++ src/main/java/com/buriti/buritiscript/model/Post.java
@@ -1,5 +1,6 @@
 package com.buriti.buritiscript.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
@@ -14,9 +15,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name="TB_POST")
-public class Post {
+public class Post implements Serializable{
 
-    @Id
+	private static final long serialVersionUID = 1L;
+
+	@Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
@@ -32,8 +35,29 @@ public class Post {
     @NotBlank
     @Lob
     private String texto;
+    
+    private String nomeImagem;
+    
+    
 
-    public Long getId() {
+    public Post() {
+		super();
+	}
+
+	public Post(Long id, @NotBlank String titulo, @NotBlank String autor, LocalDate data, @NotBlank String texto,
+			String nomeImagem) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.data = data;
+		this.texto = texto;
+		this.nomeImagem = nomeImagem;
+	}
+
+
+
+	public Long getId() {
         return id;
     }
 
@@ -72,4 +96,39 @@ public class Post {
     public void setTexto(String texto) {
         this.texto = texto;
     }
+    
+	public String getNomeImagem() {
+		return nomeImagem;
+	}
+
+	public void setNomeImagem(String nomeImagem) {
+		this.nomeImagem = nomeImagem;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+    
+    
 }
