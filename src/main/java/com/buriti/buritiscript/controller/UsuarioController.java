25fa5a81eb7b7ed5c67707a65e820bd0835df19a
Emerson66 <emersonreisdacosta@gmail.com>
2021-03-26 23:53:27 -0400

    implementado o metodo salvar usuario

--- src/main/java/com/buriti/buritiscript/controller/UsuarioController.java
+++ src/main/java/com/buriti/buritiscript/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.buriti.buritiscript.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.buriti.buritiscript.domain.model.Usuario;
+import com.buriti.buritiscript.domain.service.UsuarioService;
 
 @Controller
 @RequestMapping("usuarios")
 public class UsuarioController {
 	
-	
+	@Autowired
+	UsuarioService usuarioService;
 	
 	@GetMapping("/novo")
 	public ModelAndView novoUsuario(Usuario usuario) {	
@@ -26,7 +29,7 @@ public class UsuarioController {
 		
 		ModelAndView mv = new ModelAndView();
 		mv.addObject(usuario);
-		
+		usuarioService.save(usuario);
 		return mv;
 	}
 }
